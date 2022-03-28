29be64695bacad6d99ee119615199d799118c202
Maximiliano Gallegos <maximilianoga38@gmail.com>
2022-03-28 15:01:38 -0600

    charts avance

--- src/views/dashboard/pages/Charts/ChartType/lineChart.vue
+++ src/views/dashboard/pages/Charts/ChartType/lineChart.vue
@@ -4,110 +4,24 @@
       <v-card-title
         class="text primary center color"
         color="white"
-      >
-        Lineas
-      </v-card-title>
+      />
       <div class="my-6">
-        <column-chart
+        <area-chart
+          prefix="$"
+          thousands=","
           :data="datos"
-          download="GraficoLineas"
+          min="3000000"
         />
       </div>
     </v-card>
   </v-container>
 </template>
 <script>
-  import { db } from '@/main'
-
   export default {
-    data () {
-      return {
-        datos: [],
-        // ? prueba a intentar llenar el objeto con un ciclo usando datosLinea[i].value y datosLinea[i].nombre
-        //* no entiendo como usar la informacion que traigo de la base de datos
-        // datos: [{ name: 'Usage', data: { 20170101: 3 } }],
-        datos1: [],
-      // data: [
-      //   {
-      //     name: 'Workout',
-      //     data: {
-      //       20170101: 3,
-      //       '2017-01-02': 3,
-      //       '2017-01-03': 1,
-      //       '2017-01-04': 4,
-      //       '2017-01-05': 3,
-      //       '2017-01-06': 2,
-      //       '2017-01-07': 3,
-      //     },
-      //   },
-      //   {
-      //     name: 'Call parents',
-      //     data: {
-      //       '2017-01-01': 5,
-      //       '2017-01-02': 3,
-      //       '2017-01-03': 2,
-      //       '2017-01-04': 0,
-      //       '2017-01-05': 0,
-      //       '2017-01-06': 1,
-      //       '2017-01-07': 1,
-      //     },
-      //   },
-      // ],
-      // data: {
-      //   labels: ['A', 'B', 'C'],
-      //   series: [
-      //     [0, 6, 0],
-      //     [4, 0, 6],
-      //     [12, 6, 12],
-      //     [6, 12, 6],
-      //   ],
-      // },
-      // options: {
-      //   showPoint: false,
-      //   fullWidth: true,
-      //   lineSmooth: false,
-      //   chartPadding: {
-      //     right: 40,
-      //   },
-      //   axisX: {
-      //     showGrid: false,
-      //   },
-      // },
-      }
-    },
-    created () {
-      this.refTable()
-    },
-    methods: {
-      async refTable () {
-        try {
-          await db
-          db.collection('charts')
-            .where('cat', '==', 'bar')
-            .get()
-            .then(snap => {
-              snap.forEach(doc => {
-                this.datos.push(doc.data())
-              })
-            })
-        } catch (error) {
-          alert('Hubó un error')
-        }
-      },
+    props: {
+      datos: { type: Array, default: null },
     },
   }
-// vue.runtime.esm.js?2b0e:619 [Vue warn]: Error in data(): "TypeError: Cannot read properties of undefined (reading '0')"
-// Vue warn]: Property or method "datos" is not defined on the instance but referenced during render. Make sure that this property is reactive, either in the data option, or for class-based components, by initializing the property. See: https://vuejs.org/v2/guide/reactivity.html#Declaring-Reactive-Properties.
-// lineChart.vue?b502:81 Uncaught (in promise) TypeError: Cannot read properties of undefined (reading 'push')
-// CombinedVueInstance<Vue, {
-//     datosLinea: any[];
-//     datos: {
-//         name: string;
-//         data: {
-//             Tula: number;
-//         };
-//     }[];
-// }, unknown, unknown, Readonly<Record<never, any>>>
 </script>
 
 <style scoped>
--- src/views/dashboard/pages/Charts/bodyChart.vue
+++ src/views/dashboard/pages/Charts/bodyChart.vue
@@ -17,23 +17,61 @@
       </template>
       <cpcr-piechart />
       <cpcr-barchart />
-      <cpcr-linechart />
+      <cpcr-linechart :datos="arrayT[0]" />
+      <cpcr-linechart :datos="arrayT[1]" />
+      <v-btn @click="test()">
+        test
+      </v-btn>
     </base-material-card>
   </v-container>
 </template>
 
 <script>
+  import { mapState } from 'vuex'
+  import { db } from '@/main'
+
   import CpcrPiechart from './ChartType/pieChart.vue'
   import CpcrBarchart from './ChartType/barChart.vue'
   import CpcrLinechart from './ChartType/lineChart.vue'
-  import { mapState } from 'vuex'
+
   export default {
     components: { CpcrPiechart, CpcrBarchart, CpcrLinechart },
     data () {
-      return {}
+      return {
+        arrayT: [],
+        estima: [],
+        carteraVencida: [],
+      }
     },
     computed: {
       ...mapState(['barColor', 'barImage', 'user']),
+    },
+    created () {
+      this.refTable(0, 'estimacion')
+      this.refTable(1, 'carteraVencida')
+    },
+    methods: {
+      test () {
+        console.table(this.arrayT)
+      },
+      async refTable (arrayIndex, categoria) {
+        try {
+          await db
+          db.collection('charts')
+            .where('cat', '==', categoria)
+            .get()
+            .then(snap => {
+              const arrayData = []
+              snap.forEach(doc => {
+                arrayData.push(doc.data())
+              })
+              this.arrayT[arrayIndex] = arrayData
+              console.table(this.arrayT[arrayIndex])
+            })
+        } catch (error) {
+          alert('Hubó un error')
+        }
+      },
     },
   }
 </script>
