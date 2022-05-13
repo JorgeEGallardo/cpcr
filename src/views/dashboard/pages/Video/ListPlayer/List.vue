91e027422294cbec05391485dc9f672920632e77
Maximiliano Gallegos <maximilianoga38@gmail.com>
2022-05-13 13:47:43 -0500

    Mejora de QoL para la lsita de videos

--- src/views/dashboard/pages/Video/ListPlayer/List.vue
+++ src/views/dashboard/pages/Video/ListPlayer/List.vue
@@ -1,22 +1,39 @@
 <template>
-  <v-card
-    outlined
-    hover
-    class="center primary"
-    width="250px"
-    height="275px"
-    @click="updateVideo"
-  >
-    <v-img
-      class="center"
-      contain
-      :src="srcImg"
-      :lazy-src="srcImg"
-    />
-    <v-card-title class="d-flex text-h4 justify-center primary white--text">
-      {{ videoTitulo }}
-    </v-card-title>
-  </v-card>
+  <v-hover v-slot="{ hover }">
+    <v-card
+      outlined
+      hover
+      class="center primary"
+      width="250px"
+      height="275px"
+      @click="updateVideo"
+    >
+      <v-img
+        class="center"
+        contain
+        :src="srcImg"
+        :lazy-src="srcImg"
+      >
+        <v-fade-transition>
+          <v-overlay
+            v-if="hover"
+            :absolute="absolute"
+            color="primary"
+            :value="overlay"
+            class="d-flex transition-fast-in-fast-out v-card--reveal text-h1 white--text justify-center"
+            style="height: 100%;"
+          >
+            <v-icon x-large>
+              mdi-play-circle
+            </v-icon>
+          </v-overlay>
+        </v-fade-transition>
+      </v-img>
+      <v-card-title class="d-flex text-h4 justify-center primary white--text">
+        {{ videoTitulo }}
+      </v-card-title>
+    </v-card>
+  </v-hover>
 </template>
 
 <script>
@@ -31,6 +48,9 @@
     data () {
       return {
         srcImg: `https://img.youtube.com/vi/${this.videoid}/0.jpg`,
+        absolute: true,
+        overrlay: false,
+        opacity: 0.5,
       }
     },
     computed: {
